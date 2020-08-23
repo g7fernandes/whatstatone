454972f56eaf21936a870a28715e885b6b6ba287
Gabriel Fernandes <gafernandesil@gmail.com>
2020-08-23 20:09:56 -0300

    fix: hooks
    Some small bugfixes. The intalation path was wrong.

--- utils/install_hooks.sh
+++ utils/install_hooks.sh
@@ -2,5 +2,8 @@
 
 BASE_PATH="$(git rev-parse --show-toplevel)"
 
-ln -s $BASE_PATH/utils/pre-commit.sh $BASE_PATH/.git/hooks/pre-commit
-ln -s $BASE_PATH/utils/pre-push.sh $BASE_PATH/.git/hooks/pre-push
+ln -sf $BASE_PATH/utils/git/pre-commit.sh $BASE_PATH/.git/hooks/pre-commit
+ln -sf $BASE_PATH/utils/git/pre-push.sh $BASE_PATH/.git/hooks/pre-push
+
+chmod +x $BASE_PATH/.git/hooks/pre-push
+chmod +x $BASE_PATH/.git/hooks/pre-commit
